8c26357775b58e9722b8aeda0072691d56fbf387
Kamil Bazan <bazankamil@gmail.com>
2024-07-01 20:59:53 +0200

    added states

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+"use client";
+import React from "react";
+import Form from "./components/Form";
+import Header from "./components/Header";
+import TodoHero from "./components/TodoHero";
+import TodoList from "./components/TodoList";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Home() {
+  const [todos, setTodos] = React.useState([]);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="wrapper">
+      <Header />
+      <TodoHero todos_completed={0} total_todos={0} />
+      <Form />
+      <TodoList todos={[]} />
+    </div>
+  );
 }
-
-export default App
+export default Home;
